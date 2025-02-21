6f5c19a4526e64cffd1e895752eea8910cbe4567
NU1LSPAXE\nu1ls <82641653+nu1lspaxe@users.noreply.github.com>
2025-02-21 11:15:35 +0800

    fix: append wait group

--- Go/concurrency/channel.go
+++ Go/concurrency/channel.go
@@ -2,24 +2,31 @@ package concurrency
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func Process(ch chan int) {
+func Process(ch chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	time.Sleep(time.Second)
 	ch <- int(time.Now().Unix())
 }
 
 func CreateChannels() {
 	channels := make([]chan int, 10)
+	var wg sync.WaitGroup
 
 	for i := 0; i < 10; i++ {
 		channels[i] = make(chan int)
-		go Process(channels[i])
+		wg.Add(1)
+		go Process(channels[i], &wg)
 	}
 
 	for idx, ch := range channels {
 		<-ch
 		fmt.Printf("Routine: %v quit.\n", idx)
 	}
+
+	wg.Wait()
 }
